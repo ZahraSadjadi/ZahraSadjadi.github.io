2cf4750b91ff623037bf24287c986ca06b7e4469
ZahraSadjadi <sadjadi.1992844@studenti.uniroma1.it>
2024-06-15 11:21:54 +0100

    update gatsby

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,13 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-remark-images/gatsby-browser.js'),
+      options: {"plugins":[],"maxWidth":630,"linkImagesToOriginal":true,"showCaptions":false,"markdownCaptions":false,"backgroundColor":"white","quality":50,"withWebp":false,"withAvif":false,"loading":"lazy","decoding":"async","disableBgImageOnAlpha":false,"disableBgImage":false},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"Gatsby Starter Blog","short_name":"Gatsby","start_url":"/","background_color":"#ffffff","display":"minimal-ui","icon":"src/images/gatsby-icon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"e3a129d2a4a3f9f499b8bb80d4d98e2f"},
+    },{
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../node_modules/gatsby/dist/internal-plugins/partytown/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/page-ssr/routes/component---src-pages-index-js.js
@@ -0,0 +1,54 @@
+"use strict";
+exports.id = 293;
+exports.ids = [293];
+exports.modules = {
+
+/***/ 8917:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  Head: () => (/* binding */ Head),
+  "default": () => (/* binding */ pages)
+});
+
+// EXTERNAL MODULE: external "C:\\Users\\jafar\\Documents\\GitHub\\ZahraSadjadi.github.io\\node_modules\\react\\index.js"
+var external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_ = __webpack_require__(2763);
+// EXTERNAL MODULE: ./.cache/gatsby-browser-entry.js + 11 modules
+var gatsby_browser_entry = __webpack_require__(123);
+// EXTERNAL MODULE: ./src/components/bio.js
+var bio = __webpack_require__(4967);
+// EXTERNAL MODULE: ./src/components/layout.js
+var layout = __webpack_require__(3895);
+// EXTERNAL MODULE: ./src/components/seo.js
+var seo = __webpack_require__(7528);
+// EXTERNAL MODULE: ./node_modules/gatsby-plugin-image/dist/gatsby-image.module.js
+var gatsby_image_module = __webpack_require__(4722);
+;// CONCATENATED MODULE: ./src/components/showcase.js
+const Bio=()=>{return/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement(external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.Fragment,null,/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement("div",{className:"showcase",style:{display:'flex',alignItems:'center',flexDirection:'column',marginBottom:'20px'}},/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement(gatsby_image_module/* StaticImage */.iS,{className:"showcase-avatar",layout:"fixed",formats:["auto","webp","avif"],src:"../images/profile-pic.jpg",quality:95,alt:"Profile picture",style:{borderRadius:'50%',width:'100%',// Use 100% of the container width
+height:'auto',// Auto-adjust the height to maintain aspect ratio
+maxWidth:'250px',// Max width to prevent the image from being too large
+aspectRatio:'1 / 1'// Ensure the aspect ratio is 1:1 for a circle
+},__imageData:__webpack_require__(3086)}),/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement("div",{style:{maxWidth:'600px',textAlign:'justify'}},/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement("br",null),/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement("p",null,"An economics master's graduate excelling in business analysis and data-driven financial strategies through a robust application of economic principles."))),/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement("div",{className:"flex",style:{display:'flex',justifyContent:'center',gap:'10px'}},/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement("a",{className:"button pseudo",target:"_blank",href:"mailto:jafarzadeh.farshad@outlook.com"},/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement("i",{className:"fa fa-envelope fa-2x","aria-hidden":"true"})),/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement("a",{className:"button pseudo",target:"_blank",href:"https://www.researchgate.net/profile/Farshad_Jafarzadeh"},/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement("i",{className:"fab fa-researchgate fa-2x","aria-hidden":"true"})),/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement("a",{className:"button pseudo",target:"_blank",href:"https://www.linkedin.com/in/farshadj"},/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement("i",{className:"fab fa-linkedin-in fa-2x","aria-hidden":"true"})),/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement("a",{className:"button pseudo",target:"_blank",href:"https://scholar.google.com/citations?hl=en&user=rd7Cdq0AAAAJ"},/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement("i",{className:"ai ai-google-scholar-square ai-2x","aria-hidden":"true"}))));};/* harmony default export */ const showcase = (Bio);
+;// CONCATENATED MODULE: ./src/pages/index.js
+const BlogIndex=({data,location})=>{var _data$site$siteMetada;const siteTitle=((_data$site$siteMetada=data.site.siteMetadata)===null||_data$site$siteMetada===void 0?void 0:_data$site$siteMetada.title)||`Title`;const posts=data.allMarkdownRemark.nodes;return/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement(layout/* default */.A,{location:location,title:siteTitle},/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement(seo/* default */.A,{title:"Home"}),/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement("header",{className:"global-header"},/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement("h3",{className:"subHeader"},"Business Analyst"),/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement("br",null)),/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement(showcase,null),/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement("ol",{style:{listStyle:`none`}},posts.map(post=>{const title=post.frontmatter.title||post.fields.slug;return/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement("li",{key:post.fields.slug},/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement("article",{className:"post-list-item",itemScope:true,itemType:"http://schema.org/Article"},/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement("header",null,/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement("h2",null,/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement(gatsby_browser_entry.Link,{to:post.fields.slug,itemProp:"url"},/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement("span",{itemProp:"headline"},title))),/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement("small",null,post.frontmatter.date)),/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement("section",null,/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement("p",{dangerouslySetInnerHTML:{__html:post.frontmatter.description||post.excerpt},itemProp:"description"}))));})));};/* harmony default export */ const pages = (BlogIndex);/**
+ * Head export to define metadata for the page
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
+ */const Head=()=>/*#__PURE__*/external_C_Users_jafar_Documents_GitHub_ZahraSadjadi_github_io_node_modules_react_index_js_.createElement(seo/* default */.A,{title:"All posts"});const pageQuery="3100300281";
+
+/***/ }),
+
+/***/ 3086:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"layout":"fixed","backgroundColor":"#989898","images":{"fallback":{"src":"/ZahraSadjadi.github.io/static/a2db38f3efa52511251afc44237f64c3/b1ed6/profile-pic.jpg","srcSet":"/ZahraSadjadi.github.io/static/a2db38f3efa52511251afc44237f64c3/b1ed6/profile-pic.jpg 511w","sizes":"511px"},"sources":[{"srcSet":"/ZahraSadjadi.github.io/static/a2db38f3efa52511251afc44237f64c3/ddba2/profile-pic.avif 511w","type":"image/avif","sizes":"511px"},{"srcSet":"/ZahraSadjadi.github.io/static/a2db38f3efa52511251afc44237f64c3/6b763/profile-pic.webp 511w","type":"image/webp","sizes":"511px"}]},"width":511,"height":511}');
+
+/***/ })
+
+};
+;
+//# sourceMappingURL=component---src-pages-index-js.js.map
